3af0e82f6feb93ac6694c3802372a09ae5f1dfab
siddhant shrivastava <siddhant.shrivastava@mountblue.io>
2018-08-17 12:20:52 +0530

    after completing simple blog project using adonis js

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,102 @@
+"use strict";
+//Bring in model
+
+const Post = use("App/Models/Post");
+
+//Bring in validator
+const {validate}=use("Validator")
+class PostController {
+  async index({ view }) {
+    // const posts=[
+    //     {title:"post one", body:"this is post one"},
+    //     {title:"post two", body:"this is post two"},
+    //     {title:"post three", body:"this is post three"}
+    // ]
+
+    const posts = await Post.all();
+
+    return view.render("posts.index", {
+      title: "latest post",
+      posts: posts.toJSON()
+    });
+  }
+
+  async details({ params, view }) {
+    const post = await Post.find(params.id);
+
+    return view.render("posts.details", {
+      post: post
+    });
+  }
+
+  async add({view}){
+   return view.render("posts.add")
+  }
+
+  async store({request,response,session}){
+//validate input 
+const validation=await validate(request.all(),{
+title:'required|min:3|max:255',
+body:'required|min:3'
+}
+)
+if(validation.fails()){
+  session.withErrors(validation.messages()).flashAll()
+  return response.redirect('back')
+}
+const post=new Post()
+
+post.title=request.input("title")
+post.body=request.input("body")
+
+await post.save()
+
+session.flash({notification:"Post Added!"})
+
+return response.redirect("/posts")
+  }
+
+  async edit({params,view}){
+    const post = await Post.find(params.id);
+
+
+    return view.render("posts.edit",{
+      post:post
+    })
+   }
+
+   async update({params,request,response,session}){
+     //validate input 
+const validation=await validate(request.all(),{
+  title:'required|min:3|max:255',
+  body:'required|min:3'
+  }
+  )
+  if(validation.fails()){
+    session.withErrors(validation.messages()).flashAll()
+    return response.redirect('back')
+  }
+  const post=await Post.find(params.id)
+
+  post.title=request.input('title')
+  post.body=request.input('body')
+
+  await post.save()
+
+  session.flash({notification:'Post Updated!'})
+
+  return response.redirect('/posts')
+   }
+
+   async destroy({params,session,response}){
+     const post=await Post.find(params.id)
+
+     await post.delete()
+
+     session.flash({notification:'Post Deleted!'})
+
+     return response.redirect('/posts')
+   }
+}
+
+module.exports = PostController;
